5bc21ccf67edc3d34c207abd05860535a1ba21d9
BiancaGiovanna <biancagothicg@gmail.com>
2023-11-09 18:33:54 -0300

    model transfer

---
+++ src/main/java/com/tokiomarine/model/Transfer.java
@@ -0,0 +1,81 @@
+package com.tokiomarine.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Transfer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String contaOrigem;
+	private String contaDestino;
+	private BigDecimal valorTransferencia;
+	private BigDecimal taxa;
+	private LocalDate dataTransferencia;
+	private LocalDate dataAgendamento;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getContaOrigem() {
+		return contaOrigem;
+	}
+
+	public void setContaOrigem(String contaOrigem) {
+		this.contaOrigem = contaOrigem;
+	}
+
+	public String getContaDestino() {
+		return contaDestino;
+	}
+
+	public void setContaDestino(String contaDestino) {
+		this.contaDestino = contaDestino;
+	}
+
+	public BigDecimal getValorTransferencia() {
+		return valorTransferencia;
+	}
+
+	public void setValorTransferencia(BigDecimal valorTransferencia) {
+		this.valorTransferencia = valorTransferencia;
+	}
+
+	public BigDecimal getTaxa() {
+		return taxa;
+	}
+
+	public void setTaxa(BigDecimal taxa) {
+		this.taxa = taxa;
+	}
+
+	public LocalDate getDataTransferencia() {
+		return dataTransferencia;
+	}
+
+	public void setDataTransferencia(LocalDate dataTransferencia) {
+		this.dataTransferencia = dataTransferencia;
+	}
+
+	public LocalDate getDataAgendamento() {
+		return dataAgendamento;
+	}
+
+	public void setDataAgendamento(LocalDate dataAgendamento) {
+		this.dataAgendamento = dataAgendamento;
+	}
+
+}
