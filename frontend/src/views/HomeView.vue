80ce8ba9b04ac08f4cc8eab1ae619a1587d63c1d
BiancaGiovanna <biancagothicg@gmail.com>
2023-11-10 15:43:18 -0300

    ajusting erros frontend

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,11 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home"></div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HomeView",
+});
+</script>
