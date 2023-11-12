f4edc74fffaf66f599d08279b60e91b6368040b3
BiancaGiovanna <biancagothicg@gmail.com>
2023-11-12 16:46:15 -0300

    Adjusment headerComponent

--- frontend/src/components/HeaderComponent.vue
+++ frontend/src/components/HeaderComponent.vue
@@ -1,25 +1,7 @@
-<!-- HeaderComponent.vue -->
 <template>
   <header class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-      <router-link to="/" class="navbar-brand">Meu Aplicativo</router-link>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-            <router-link to="/" class="nav-link">Página Inicial</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/about" class="nav-link">Sobre</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/contact" class="nav-link">Contato</router-link>
-          </li>
-        </ul>
-      </div>
+      <router-link to="/" class="navbar-brand">Agendamento de transferências</router-link>
     </div>
   </header>
 </template>
